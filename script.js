2b7b27613722099df8cca9b3a432749bc67d71e5
mendesdafilla <dafila.mendes@alunoescolassociais.org.br>
2024-10-25 08:59:36 -0300

    altera js

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
     const valor = parseFloat(document.getElementById('amount').valeu);
     const daMoeda = document.getElementById('daMoeda').value;
     const paraMoeda = document.getElementById('paraMoeda').value;
-
+//definir taxa de cambio fixa 
+ const exchangeRates = {
+    USD:{ BRL: 5.67 , EUR: 0.92 },
+    BRL:{ USD: 0.18, EUR: 0.16 },
+    EUR:{ USD: 1.08, BRL: 6.16 },
+ };
 //conversão simples
 let valorConvertido
 if(daMoeda === paraMoeda){
     valorConvertido = valor;
 }else{
-    
+    valorConvertido = valor * exchangeRates[daMoeda][paraMoeda];
 }
-   
+ //Exibir resultado
+ const conversão = document.getElementById('conversao');
+ conversao.textContent = `Resultado: ${valorConvertido}`;
+
 });
